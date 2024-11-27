a51b82d62b1d60a6b98a3144ad7129a7cc05c75b
AidaBur <aidaburlutckaia@gmail.com>
2024-11-26 22:39:21 -0500

    Completed week 4 assignment: moved frontend files to frontend-public directory, updated static file serving path in app.js, refactored routes and controllers for managing people data, added functionalities for adding, getting, updating, and deleting people.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const path = require("path");
-const { products } = require("./data");
-console.log("Express Tutorial");
+const { products, people } = require("./data");  
+const peopleRouter = require("./routes/people");
 
 const app = express();
-app.use(express.static("./public"));
+console.log("Express Tutorial");
+
+app.use(express.static("./frontend-public"));
+
+
+// Middleware logger
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().toISOString();
+
+  console.log(`${method} ${url} at ${time}`);
+  next();
+};
+
+app.use(logger);
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+});
+
 
 app.get("/api/v1/products", (req, res) => {
   res.json(products);
 });
+
 
 app.get("/api/v1/products/:productID", (req, res) => {
   const idToFind = parseInt(req.params.productID);
@@ -19,6 +44,7 @@ app.get("/api/v1/products/:productID", (req, res) => {
     res.status(404).json({ message: "That product was not found" });
   }
 });
+
 
 app.get("/api/v1/query", (req, res) => {
   const { search, limit, priceLessThan, regex } = req.query;
@@ -50,13 +76,34 @@ app.get("/api/v1/query", (req, res) => {
   res.json(filteredProducts);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+
+app.get("/api/v1/people", (req, res) => {
+  res.json(people);
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("resourse not found");
+
+app.post("/api/v1/people", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ success: false, message: "Please provide a name" });
+  }
+
+  
+  const newPerson = {
+    id: people.length + 1,  
+    name: req.body.name,   
+  };
+
+  people.push(newPerson);
+
+  
+  res.status(201).json({ success: true, name: req.body.name });
 });
+
+
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000....");
