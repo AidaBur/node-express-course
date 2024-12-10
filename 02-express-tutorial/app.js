c978555a65dd8c01ec9886f21df69922aab01aea
AidaBur <aidaburlutckaia@gmail.com>
2024-12-09 19:51:27 -0500

    Fixed people API routing in app.js.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const path = require("path");
-const { products, people } = require("./data");  
+const { products, people } = require("./data");
 const peopleRouter = require("./routes/people");
 
 const app = express();
 console.log("Express Tutorial");
 
 app.use(express.static("./public"));
-
 
 // Middleware logger
 const logger = (req, res, next) => {
@@ -21,17 +20,11 @@ const logger = (req, res, next) => {
 
 app.use(logger);
 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./public/index.html"));
-});
-
-
-app.use("/api/v1/products", (req, res) => {
-  res.json(products);
 });
 
 app.get("/api/v1/products", (req, res) => {
@@ -47,7 +40,6 @@ app.get("/api/v1/products/:productID", (req, res) => {
     res.status(404).json({ message: "That product was not found" });
   }
 });
-
 
 app.get("/api/v1/query", (req, res) => {
   const { search, limit, priceLessThan, regex } = req.query;
@@ -79,11 +71,11 @@ app.get("/api/v1/query", (req, res) => {
   res.json(filteredProducts);
 });
 
+app.use("/api/v1/people", peopleRouter);
 
 app.all("*", (req, res) => {
   res.status(404).send("resource not found");
 });
-
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000....");
